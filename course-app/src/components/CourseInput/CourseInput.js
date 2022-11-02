457873e2cd55674fa22ab45af20577041053098e
Asrasadik <47492644+Asrasadik@users.noreply.github.com>
2022-11-02 23:49:23 +0530

    Validating input

--- course-app/src/components/CourseInput/CourseInput.js
+++ course-app/src/components/CourseInput/CourseInput.js
@@ -4,6 +4,7 @@ import Button from "../UI/Button";
 
 const CourseInput = (props) => {
   const [inputValue, setInputValue] = useState("");
+  const [isValid, setValid] = useState(true);
   const InputChange = (e) => {
     //console.log(e.target.value);
     setInputValue(e.target.value);
@@ -11,13 +12,21 @@ const CourseInput = (props) => {
   const formSubmit = (event) => {
     event.preventDefault();
     //send value to list items
+    if (inputValue.trim().length === 0) {
+      setValid(false);
+      return;
+    }
     props.onAddItem(inputValue);
   };
   return (
     <form onSubmit={formSubmit}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={InputChange} />
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          style={{ borderColor: !isValid ? "red" : "#8b005d" }}
+          type="text"
+          onChange={InputChange}
+        />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
